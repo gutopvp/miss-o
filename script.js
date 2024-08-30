2ec8bff0b6a810c295eb1e4bb8f98a5d79f85c2d
gutopvp <108087931+gutopvp@users.noreply.github.com>
2024-08-30 07:29:46 -0300

    dinhero

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-pergunta");
-const caixaAlternativa = document.querySelector(".caixa-alternativa");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
@@ -9,29 +9,50 @@ enunciado: "Qual é uma prática benéfica para promover o bem-estar fisíco e m
 alternativas: [
 {
 texto:"Passeios diários ao ar livre",
-afirmação:"O exercício regular é crucial para manter a saúde cardiovascular e a agilidade fisíca em humanos e animais"
+afirmacao:"O exercício regular é crucial para manter a saúde cardiovascular e a agilidade fisíca em humanos e animais"
 }, 
 {
-texto:"Assitir televisão juntos"
-afirmação:"Interagir e brincar com animais de estimação pode reduzir os níveis de estresse e promover uma sensação de calma e felicidade tanto para as pessoas quanto para os animais"
+texto:"Assitir televisão juntos",
+afirmacao:"Interagir e brincar com animais de estimação pode reduzir os níveis de estresse e promover uma sensação de calma e felicidade tanto para as pessoas quanto para os animais"
 } 
 ]    
 }
 ];
 let atual = 0;
 let perguntaAtual;
-function mostraPergunta(){ 
-perguntaAtual = perguntas[atual];
-caixaPergutas.textContent = perguntaAtual.enunciado;
-caixaAlternativa.textContent = "";
-mostraAlternativas();
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    textoResultado.textContent = "";
+    mostraAlternativas();
 }
+
 function mostraAlternativas(){
-for (const alternativa of perguntaAtual.alternativa){
-const botaoAltenativas = document.createElement("button");
-botaoAltenativa.textContent = alternativa;
-caixaAltenativa.appendChild(botaoAltenativa);
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener('click', ()=> respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);        
+    }
 }
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacao = opcaoSelecionada.afirmacao;
+    historiaFinal = afirmacao;
+    atual++
+    mostraPergunta();
 }
+
 mostraPergunta();
-    
+
+function mostraResultado(){
+    caixaPerguntas.textContent = "Em 2030...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
